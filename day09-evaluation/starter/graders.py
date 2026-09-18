6ec75d43d4fe92f79054042200ce7da610b60327
Aditya bhatia <aditya89bh@gmail.com>
2026-09-18 14:29:35 +0300

    Add Day 9 evaluation and testing content

---
+++ day09-evaluation/starter/graders.py
@@ -0,0 +1,23 @@
+def grade_task_success(result, case):
+    # TODO
+    raise NotImplementedError
+
+
+def grade_budget(result, case):
+    # TODO
+    raise NotImplementedError
+
+
+def grade_required_tool(trace, case):
+    # TODO
+    raise NotImplementedError
+
+
+def grade_forbidden_tool(trace, case):
+    # TODO
+    raise NotImplementedError
+
+
+def grade_max_steps(trace, case):
+    # TODO
+    raise NotImplementedError
